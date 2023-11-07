05b62e82eb873b934f24a5af6aa049b6bdda7f28
eeeunbin <149546590+eeeunbin@users.noreply.github.com>
2023-11-07 11:25:45 +0900

    Add files via upload

--- js/eb-pf.js
+++ js/eb-pf.js
@@ -33,4 +33,21 @@ $(document).ready(function(){
       clickable: true,
     },
   });
+
+  var swiper = new Swiper(".firstSwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 })
